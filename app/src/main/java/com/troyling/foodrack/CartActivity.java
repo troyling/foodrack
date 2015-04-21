9b4f7b252f5fd91f01afe14d44e9bfbc8009d925
Troy Ling <troyling14@gmail.com>
2015-04-20 21:55:44 -0400

    add credit card processing support

--- app/src/main/java/com/troyling/foodrack/CartActivity.java
+++ app/src/main/java/com/troyling/foodrack/CartActivity.java
@@ -3,28 +3,72 @@ package com.troyling.foodrack;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.braintreepayments.api.dropin.BraintreePaymentActivity;
+import com.foodrack.helpers.ErrorHelper;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import org.apache.http.Header;
+
 /**
  * Created by ChandlerWu on 4/17/15.
  */
 public class CartActivity extends ActionBarActivity {
+    private final int REQUEST_CODE = 100;
     Button payButton;
+    String clientToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_layout);
 
-        payButton = (Button)this.findViewById(R.id.buttonPay);
+        getTokenFromServer();
+
+        payButton = (Button) this.findViewById(R.id.buttonPay);
         payButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // To be implemented;
+                Intent intent = new Intent(CartActivity.this, BraintreePaymentActivity.class);
+                intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, clientToken);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
+    }
+
+    private void getTokenFromServer() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://foodrackserver.herokuapp.com/transaction/client_token", new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int i, Header[] headers, String s, Throwable throwable) {
+                Log.e("Error...", "Connecting server");
+            }
+
+            @Override
+            public void onSuccess(int i, Header[] headers, String s) {
+                clientToken = s;
+                ErrorHelper.getInstance().promptError(CartActivity.this, "Get", clientToken);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == BraintreePaymentActivity.RESULT_OK) {
+                String paymentMethodNonce = data.getStringExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
+                ErrorHelper.getInstance().promptError(this, "Nonce", paymentMethodNonce);
+                postNonceToServer(paymentMethodNonce);
+            }
+        }
     }
 
     @Override
@@ -32,6 +76,27 @@ public class CartActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_welcome, menu);
         return true;
+    }
+
+    private void postNonceToServer(String paymentMethodNonce) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("payment_method_nonce", paymentMethodNonce);
+        // TODO reflect the amount here
+        params.put("amount", 3);
+        client.post("https://foodrackserver.herokuapp.com/transaction/purchase", params,
+            new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                    ErrorHelper.getInstance().promptError(CartActivity.this, "Success", "Successfully processed credit card.");
+                }
+
+                @Override
+                public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                    ErrorHelper.getInstance().promptError(CartActivity.this, "Fail", throwable.getMessage());
+                }
+            }
+        );
     }
 
     @Override
--- app/src/main/java/com/troyling/foodrack/menu3_Fragment.java
+++ app/src/main/java/com/troyling/foodrack/menu3_Fragment.java
@@ -2,22 +2,22 @@ package com.troyling.foodrack;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
 
 /**
  * Created by ChandlerWu on 4/13/15.
  */
 public class menu3_Fragment extends Fragment {
     View rootView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_account, container, false);
         return rootView;
-
     }
 
 }
