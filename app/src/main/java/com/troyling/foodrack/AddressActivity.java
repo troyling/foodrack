c8c61fcff2fc79f47cca17302be8d7a2d9f573b0
Zhouxiao Wu <zwu@wpi.edu>
2015-04-30 21:25:28 -0400

    Add mapview to address activity

--- app/src/main/java/com/troyling/foodrack/AddressActivity.java
+++ app/src/main/java/com/troyling/foodrack/AddressActivity.java
@@ -3,29 +3,83 @@ package com.troyling.foodrack;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by ChandlerWu on 4/17/15.
  */
 public class AddressActivity extends ActionBarActivity {
+
     Button confirmButton;
+    LatLng mll;
+    Marker mMarker;
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_layout);
 
+        setUpMapIfNeeded();
+        Log.d("Maker's Old location", "is" + mMarker.getPosition().latitude + mMarker.getPosition().longitude);
+
+
         confirmButton = (Button)this.findViewById(R.id.buttonConfirm);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("Maker's New location", "is" + mMarker.getPosition().latitude + mMarker.getPosition().longitude);
                 Intent intent = new Intent(AddressActivity.this, CartActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAddress))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        // Get current location
+        mMap.getMyLocation();
+        // Hard coded the location for the marker
+        mll = new LatLng(42.2743400,-71.8097730);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mll, 15));
+        mMarker = mMap.addMarker(new MarkerOptions().position(mll).title("Marker").draggable(true));
+
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                mMarker.setPosition(cameraPosition.target);
+            }
+        });
+
+
     }
 
     @Override
